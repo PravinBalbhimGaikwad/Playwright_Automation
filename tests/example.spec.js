b835dd015480da726fb5ad4269328a6a551c6c75
Gaikwad Pravin <pravingaikwad5860@gmail.com>
2026-03-26 11:01:27 +0530

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,17 @@
 // @ts-check
+//Case1 - Calling through browser
+const test = Require('@playwright/test')
+test('Browser context page', await ({Browser})=>
+  {
+    const context = await Browser.NewContext();
+    const page = context.newPage();
+  }
+    );
+
+// Case 2
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+test('page context browser', await ({page})==>
+     {
+ Page.goto('https://www.google.com');
+}
